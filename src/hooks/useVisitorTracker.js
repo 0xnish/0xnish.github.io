0242eb3e2ae96afc011fad5bba7cf72b37f7240e
Nishanth JP <mailtonishii@gmail.com>
2026-07-31 19:07:27 +0530

    Drop device model, OS, and referrer from visitor tracking

--- src/hooks/useVisitorTracker.js
+++ src/hooks/useVisitorTracker.js
@@ -3,42 +3,6 @@ import { useEffect } from 'react'
 const ENDPOINT =
   'https://script.google.com/macros/s/AKfycbxDzQktCrxKCDtLZG17V3bLf5nzV9WDwQcjHrsQz4rMFMd3dm2CgTqPF2AcgyAIuLbY/exec'
 
-function detectOs(ua) {
-  const android = ua.match(/Android ([\d.]+)/)
-  if (android) return `Android ${android[1]}`
-  if (/iPhone|iPad|iPod/.test(ua)) {
-    const m = ua.match(/OS ([\d_]+)/)
-    return m ? `iOS ${m[1].replace(/_/g, '.')}` : 'iOS'
-  }
-  if (/Windows/.test(ua)) {
-    const m = ua.match(/Windows NT ([\d.]+)/)
-    const names = {
-      '10.0': 'Windows 10',
-      '6.3': 'Windows 8.1',
-      '6.2': 'Windows 8',
-      '6.1': 'Windows 7',
-      '6.0': 'Windows Vista',
-      '5.1': 'Windows XP',
-    }
-    return m ? names[m[1]] || `Windows ${m[1]}` : 'Windows'
-  }
-  if (/Mac OS X/.test(ua)) {
-    const m = ua.match(/Mac OS X ([\d_.]+)/)
-    return m ? `macOS ${m[1].replace(/_/g, '.')}` : 'macOS'
-  }
-  if (/CrOS/.test(ua)) return 'Chrome OS'
-  if (/Linux/.test(ua)) return 'Linux'
-  return ''
-}
-
-function detectDeviceModel(ua) {
-  if (/iPhone/.test(ua)) return 'iPhone'
-  if (/iPad/.test(ua)) return 'iPad'
-  if (/Macintosh/.test(ua)) return 'Macintosh'
-  const m = ua.match(/; ?([^;]+) Build\//)
-  return m ? m[1].trim() : ''
-}
-
 function detectDevice() {
   const ua = navigator.userAgent
   const device = /Mobi|Android|iPhone/i.test(ua)
@@ -46,11 +10,9 @@ function detectDevice() {
       ? 'Tablet'
       : 'Mobile'
     : 'Desktop'
-  const os = detectOs(ua)
   const browserMatch = ua.match(/(Edg|Chrome|Firefox|Safari|OPR|MSIE)[/\s]([\d.]+)/)
   const browser = browserMatch ? `${browserMatch[1]} ${browserMatch[2]}` : 'Unknown'
-  const deviceModel = detectDeviceModel(ua)
-  return { device, deviceModel, os, browser }
+  return { device, browser }
 }
 
 async function getIpInfo() {
@@ -77,9 +39,8 @@ export default function useVisitorTracker() {
     sessionStorage.setItem('vt', '1')
 
     ;(async () => {
-      const referrer = document.referrer || ''
       const info = await getIpInfo()
-      const { device, deviceModel, os, browser } = detectDevice()
+      const { device, browser } = detectDevice()
       fetch(ENDPOINT, {
         method: 'POST',
         mode: 'no-cors',
@@ -88,11 +49,8 @@ export default function useVisitorTracker() {
           ...info,
           language: navigator.language || '',
           device,
-          deviceModel,
-          os,
           browser,
           screen: `${screen.width}x${screen.height}`,
-          referrer,
         }),
       }).catch(() => {
         // tracking is best-effort, never block the page
